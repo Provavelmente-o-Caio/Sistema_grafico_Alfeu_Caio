entrega 1.7: final rotation fix

models/point_3d.py
@@ -46,12 +46,12 @@ class Point3D:
         new_coords = []
         x, y, z = self.coordinates
         point = np.matrix([x, y, z, 1])
-        transformed_point = point * op
+        transformed_point = point @ op
         new_coords.append(
             (
                 float(transformed_point[0, 0]),
                 float(transformed_point[0, 1]),
-                float(transformed_point[0, 2]),
+                float(transformed_point[0, 2])
             )
         )
         if len(new_coords) == 1:

models/wireframe.py
@@ -52,7 +52,7 @@ class Wireframe:
         new_coords = []
         for x, y in self.coordinates:
             point = np.matrix([x, y, 1])
-            transformed_point = point * op
+            transformed_point = point @ op
             new_coords.append(
                 (float(transformed_point[0, 0]), float(transformed_point[0, 1]))
             )

utils/transformations.py
@@ -69,13 +69,9 @@ def create_rotation_matrix_3dz(angle: float) -> np.matrix:
     ])
 
 def create_rotation_matrix_3d(x_angle: float, y_angle: float, z_angle: float) -> np.matrix:
-    x_angle = np.deg2rad(x_angle)
-    y_angle = np.deg2rad(y_angle)
-    z_angle = np.deg2rad(z_angle)
-
     Rx = create_rotation_matrix_3dx(x_angle)
-    Ry = create_rotation_matrix_3dz(y_angle)
-    Rz = create_rotation_matrix_3dy(z_angle)
+    Ry = create_rotation_matrix_3dy(y_angle)
+    Rz = create_rotation_matrix_3dz(z_angle)
 
     R = Rx @ Ry @ Rz
     return R